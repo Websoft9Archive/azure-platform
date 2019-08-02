ade1b3cb758e769f7f641c6c3c9f48c3f0919b75
darren chen <chendelin1982@126.com>
2019-08-02 14:02:14 +0800

    docs

---
+++ docs/.vuepress/sidebar/zh.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    title: '开始',
+    collapsable: false,
+    children: [
+      '/zh/',
+      '/zh/stack-deployment',
+      '/zh/stack-accounts',
+    ]
+  },
+  
+  {
+    title: '云服务器（VM）',
+    collapsable: false,
+    children: [
+      '/zh/server-login',
+      '/zh/server-create',
+      '/zh/server-backup',
+      '/zh/server-extension',
+      '/zh/server-upgrade',
+      '/zh/server-snapshot-image',
+      '/zh/server-disk',
+    ]
+  },
+  
+  {
+    title: '网络与安全',
+    collapsable: false,
+    children: [
+      '/zh/network-ip',
+      '/zh/network-safegroup',
+      '/zh/network-dns',
+    ]
+  },
+  
+  {
+    title: '商务',
+    collapsable: false,
+    children: [
+      '/zh/business-marketplace',
+      '/zh/business-fee',
+      '/zh/business-buy',
+      '/zh/business-order',
+    ]
+  },
+
+  {
+    title: '其他',
+    collapsable: false,
+    children: [
+      '/zh/else-faq',
+      '/zh/else-troubleshooting',
+    ]
+  },
+  
+  ]
--- docs/zh/README.md
+++ docs/zh/README.md
@@ -1,5 +1,2 @@
 # azure-platform
-
 Using the Images of Websoft9, you should have some skills on Azure
-
-使用Websoft9镜像过程中，您需要掌握的Azure平台的一些技能
